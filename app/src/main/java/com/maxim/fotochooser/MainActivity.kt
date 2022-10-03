1727d609200623ced50f5cbfabf9412b263302ac
Maksym <hommays1995@gmail.com>
2022-10-03 21:38:44 +0300

    added function to button that pick picture from gallery

--- app/src/main/java/com/maxim/fotochooser/MainActivity.kt
+++ app/src/main/java/com/maxim/fotochooser/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.maxim.fotochooser
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var button: Button
+    private lateinit var imageView: ImageView
+
+    companion object {
+        const val IMAGE_REQUEST_CODE = 100
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.chose_picture)
+        imageView = findViewById(R.id.chosen_picture)
+
+        button.setOnClickListener{
+            pickImageFromGallery()
+        }
+    }
+    private fun pickImageFromGallery(){
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == IMAGE_REQUEST_CODE && resultCode == RESULT_OK)
+            imageView.setImageURI(data?.data)
     }
 }
